f15e8f60a82fc65685775315b6eedbba97b490a4
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-22 15:51:58 -0500

    getFullYear()

---
+++ app.js
@@ -0,0 +1,12 @@
+/****************************************************
+*                  The getTime() Method             *
+****************************************************/
+var d = new Date();
+document.getElementById("demo").innerHTML = d.getTime();
+
+/****************************************************
+*              The getFullYear() Method             *
+****************************************************/
+var d = new Date();
+document.getElementById("get-time").innerHTML = d.getFullYear();
+console.log(Date());
