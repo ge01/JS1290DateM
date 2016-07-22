2efbfc380d7be974894a78cd6fd95df34d9e50aa
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-22 16:01:59 -0500

    getDay() Method

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ document.getElementById("demo").innerHTML = d.getTime();
 *              The getFullYear() Method             *
 ****************************************************/
 var d = new Date();
-document.getElementById("get-time").innerHTML = d.getFullYear();
+document.getElementById("get-year").innerHTML = d.getFullYear();
+console.log(Date());
+
+/****************************************************
+*                The getDay() Method                *
+****************************************************/
+var d = new Date();
+document.getElementById("get-day").innerHTML = d.getDay();
 console.log(Date());
